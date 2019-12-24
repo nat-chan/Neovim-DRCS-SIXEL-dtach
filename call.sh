8cc762c640402f1e5f3baf3e57a0427d49548dcc
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2019-12-24 21:05:58 +0900

    fix api url

--- call.sh
+++ call.sh
@@ -3,5 +3,5 @@
 curl -X POST -H "Authorization: token $GITHUB_TOKEN" \
              -H "Accept: application/vnd.github.everest-preview+json"  \
              -H "Content-Type: application/json" \
-             https://api.github.com/repos/:nat-chan/:hello_actions/dispatches \
+             https://api.github.com/repos/nat-chan/hello_actions/dispatches \
              --data '{"event_type":"test_trigger"}'
