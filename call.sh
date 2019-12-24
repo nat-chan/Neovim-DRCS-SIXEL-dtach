5e814ff2e015f4c826736925e8d8d8be670f57f2
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2019-12-24 21:03:21 +0900

    change hooked event

--- call.sh
+++ call.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -X POST -H "Authorization: token $GITHUB_TOKEN" \
-             -H "Accept: application/vnd.github.ant-man-preview+json"  \
+             -H "Accept: application/vnd.github.everest-preview+json"  \
              -H "Content-Type: application/json" \
              https://api.github.com/repos/:nat-chan/:hello_actions/dispatches \
              --data '{"event_type":"test_trigger"}'
