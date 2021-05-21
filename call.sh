996d8dc4e65512027fddc7d3c04076917cec7760
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2021-05-21 17:23:22 +0900

    event_typeで発火するrepository_dispatch対象を分ける

--- call.sh
+++ call.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-curl -X POST -H "Authorization: token $GITHUB_TOKEN" \
-             -H "Accept: application/vnd.github.everest-preview+json"  \
-             -H "Content-Type: application/json" \
-             https://api.github.com/repos/$GITHUB_REPOSITORY/dispatches \
-             --data '{"event_type":"test_trigger"}'
+# stable_trigger
+# nightly_trigger
+event_type=${1:-nightly_trigger}
+
+curl -X POST \
+     -H "Authorization: token $GITHUB_TOKEN" \
+     -H "Accept: application/vnd.github.everest-preview+json"  \
+     -H "Content-Type: application/json" \
+     https://api.github.com/repos/$GITHUB_REPOSITORY/dispatches \
+     --data "{\"event_type\":\"${event_type}\"}"
